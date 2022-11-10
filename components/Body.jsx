d6a21680885c8dc747d32dadcac1822b55cbdd68
Julio Rojas <juliorojas81871@gmail.com>
2022-11-10 18:43:37 -0500

    added search

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,9 +1,13 @@
-const Body = () => {
-  return (
-    <div>
-      Body
-    </div>
-  )
-}
+import { Search } from "./";
+import { useState } from "react";
 
-export default Body
+const Body = () => {
+  const [search, setSearch] = useState("");
+  return (
+    <section className="bg-black ml-24 py-4 space-y-8 md:max-w-6xl flex-grow md:mr-2.5">
+      <Search search={search} setSearch={setSearch} />
+    </section>
+  );
+};
+
+export default Body;
