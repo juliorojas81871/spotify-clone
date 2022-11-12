8617a0a56d201f9632e191156c2b80be5e249f3a
Julio Rojas <juliorojas81871@gmail.com>
2022-11-11 20:26:15 -0500

    login

--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Sidebar } from './Sidebar';
 export { default as Body } from './Body';
 export { default as Right } from './Right';
 export { default as Search } from './Search';
+export { default as Loader } from './Loader';
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,29 @@
 import Head from "next/head";
 import { Dashboard } from "../components";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import {Loader} from '../components'
 
 const Home = () => {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push("/auth/signin");
+    },
+  });
+
+  // Loading animation...
+  if (status === "loading") {
+    return <Loader />;
+  }
+
   return (
     <div>
       <Head>
         <title>Sportify Clone - Dashboard</title>
         <link rel="icon" href="/spotify.png" />
       </Head>
-      {/* dashboard */}
       <Dashboard />
     </div>
   );
